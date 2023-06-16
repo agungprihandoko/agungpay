2a7063273d8388496a1577c2a9f3b187d4a7f9b4
agungprihandoko <11220868agungprihandoko@gmail.com>
2023-06-16 22:42:18 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<br> <br> <br> <br>  
+<!DOCTYPE html>
+<html>
+<head>
+	<title>AGUNG PAY</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+<body style="background-color: #eeeeee;">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<h3>Silahkan Pilih Menu Login</h3>
+
+						<a href="http://127.0.0.1/agungpay/agen/index.php">
+							<img src="images/logoagungpay.png" width="150px" >
+						</a>
+
+						<a href="http://127.0.0.1/agungpay/pln/index.php">
+							<img src="images/logo_pln.png" width="150px" >
+						</a>
+
+					</div>
+					<!-- footer -->
+					<div class="panel-footer">
+							<center>&copy;<?php echo date("Y"); ?> - AGUNG PRIHANDOKO</center>
+						</div>
+						<!-- end footer -->
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
